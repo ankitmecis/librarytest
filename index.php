8c42d30a092c71f5f585ceb0973cd72cc92b43ae
ankitmecis <54885737+ankitmecis@users.noreply.github.com>
2019-09-07 15:00:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php
- phpinfo();
-?>
+<html>
+<head>
+<title><?php echo getenv('CLUSTER_NAME'); ?></title>
+<head>
+<body style='text-align:center;'>
+<h1>Welcome to <?php echo getenv('CLUSTER_NAME'); ?></h1>
+<body>
+</html>
